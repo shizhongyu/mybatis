d23d2833aabaa0f8a347d1b303d790a6f3ffe740
lizhongyu <1966957866@qq.com>
2017-10-11 17:50:08 +0800

    e3mall day04 success.

--- e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
+++ e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -1,16 +1,23 @@
 package cn.e3mall.service.impl;
 
-import cn.e3mall.common.pojo.EasyUIDataGridResult;
-import cn.e3mall.pojo.TbItem;
-import cn.e3mall.pojo.TbItemExample;
-import cn.e3mall.pojo.TbItemExample.Criteria;
-import cn.e3mall.service.ItemService;
-import cn.e3mall.mapper.TbItemMapper;
-import com.github.pagehelper.PageHelper;
-import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import cn.e3mall.common.pojo.EasyUIDataGridResult;
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.common.utils.IDUtils;
+import cn.e3mall.mapper.TbItemDescMapper;
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.pojo.TbItemDesc;
+import cn.e3mall.pojo.TbItemExample;
+import cn.e3mall.pojo.TbItemExample.Criteria;
+import cn.e3mall.service.ItemService;
+
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,7 +32,8 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
-	
+	@Autowired
+	private TbItemDescMapper itemDescMapper;
 	@Override
 	public TbItem getItemById(long itemId) {
 		//根据主键查询
@@ -60,4 +68,28 @@ public class ItemServiceImpl implements ItemService {
 		return result;
 	}
 
+	@Override
+	public E3Result addItem(TbItem item, String desc) {
+		//生成商品id
+		long itemId = IDUtils.genItemId();
+		//补全item的属性
+		item.setId(itemId);
+		//1-正常，2-下架，3-删除
+		item.setStatus((byte) 1);
+		item.setCreated(new Date());
+		item.setUpdated(new Date());
+		//向商品表插入数据
+		itemMapper.insert(item);
+		//创建一个商品描述表对应的pojo对象。
+		TbItemDesc itemDesc = new TbItemDesc();
+		//补全属性
+		itemDesc.setItemId(itemId);
+		itemDesc.setItemDesc(desc);
+		itemDesc.setCreated(new Date());
+		itemDesc.setUpdated(new Date());
+		//向商品描述表插入数据
+		itemDescMapper.insert(itemDesc);
+		//返回成功
+		return E3Result.ok();
+	}
 }
